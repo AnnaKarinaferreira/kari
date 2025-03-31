2fb327d6ee660bf02b45960acb7d77f50a2c938d
annakarina <annapacheco203@gmail.com>
2025-03-31 04:25:48 -0300

    upgrade mudança de button e aplicação de umas skills

--- src/Components/Habilidades/Habilidades.jsx
+++ src/Components/Habilidades/Habilidades.jsx
@@ -3,7 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './Habilidades.css';
 import Header from '../Header/Header';
 import react from '../imgs/react.svg';
-
+import bootstrap from '../imgs/bootstrap.svg'
+import node from '../imgs/node.svg'
 
 const Habilidades = () => {
     return(
@@ -15,9 +16,16 @@ const Habilidades = () => {
         <div className='linha1'>
             <div> 
                 <img src={react} className='react' />
+                <p className='namereact'>R E A C T</p>
             </div>
-            <div className='bootstrap'></div>
-            <div className='nodejs'></div>
+            <div className='bootstrap'>
+                <img src={bootstrap} className='bootstrap' />
+                <p className='namebootstrap'>B O O T S T R A P</p>
+            </div>
+            <div className='nodejs'>
+                <img src={node} className='node'/>
+                <p className='namenode'>N O D E . J S </p>
+            </div>
         </div>
         </>
     )
